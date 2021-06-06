79e659b0e1f8797debbff044f77e9bed0d9d4f78
Miller777 <miller777@mail.ru>
2021-06-07 02:59:18 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/flweather/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flweather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Weather',
+            style: TextStyle(
+              color: Colors.black87,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          iconTheme: IconThemeData(color: Colors.black54),
+          brightness: Brightness.light,
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+        body: _buildBody(),
+      ),
+    );
+  }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        _headerImage(),
+      ],
+    ),
+  );
+}
+
+Image _headerImage() {
+  return Image(
+    image: NetworkImage(
+        'https://elektro-sadovnik.ru/wp-content/uploads/2020/01/3-kakaya-budet-vesna.jpg'),
+    fit: BoxFit.cover,
+  );
+}
