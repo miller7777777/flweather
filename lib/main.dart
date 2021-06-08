b81caf4f4d13440c4b02d638b5d9afab8763a026
Miller777 <miller777@mail.ru>
2021-06-09 01:39:16 +0300

    complite;

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,23 @@ Widget _buildBody() {
     child: Column(
       children: [
         _headerImage(),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                _weatherDescription(),
+                Divider(),
+                _temperature(),
+                Divider(),
+                _temperatureForecast(),
+                Divider(),
+                _footerRatings(),
+              ],
+            ),
+          ),
+        ),
       ],
     ),
   );
@@ -50,5 +67,143 @@ Image _headerImage() {
     image: NetworkImage(
         'https://elektro-sadovnik.ru/wp-content/uploads/2020/01/3-kakaya-budet-vesna.jpg'),
     fit: BoxFit.cover,
+  );
+}
+
+Column _weatherDescription() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        'Tuesday - May 22',
+        style: TextStyle(
+          fontSize: 32.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Divider(),
+      Text(
+        'Князь плавно поднял руку, покрутил пальцами, показывая пустую ладонь, затем сжал кисть в кулак – так же медленно. Прикрыл глаза, концентрируясь. И вдруг рука исчезла с коротким сухим треском, а в столешнице возник аккуратный пролом длиной в полметра – по крайней мере, так это выглядело для изумлённых творцов.',
+        style: TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+    ],
+  );
+}
+
+Row _temperature() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      SizedBox(
+        width: 16.0,
+      ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text(
+                '15 Clear',
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Text(
+                'Murmanskaya oblast, Murmansk',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      )
+    ],
+  );
+}
+
+Wrap _temperatureForecast() {
+  return Wrap(
+    spacing: 8.0,
+    children: List.generate(
+      8,
+      (index) => Chip(
+        label: Text(
+          '${index + 20} °С',
+          style: TextStyle(
+            fontSize: 12.0,
+          ),
+        ),
+        avatar: Icon(
+          Icons.wb_cloudy,
+          color: Colors.blue.shade300,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4.0),
+          side: BorderSide(color: Colors.grey),
+        ),
+        backgroundColor: Colors.grey.shade100,
+      ),
+    ),
+  );
+}
+
+Row _footerRatings() {
+  var stars = Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      Icon(
+        Icons.star,
+        size: 15.0,
+        color: Colors.yellow[600],
+      ),
+      Icon(
+        Icons.star,
+        size: 15.0,
+        color: Colors.yellow[600],
+      ),
+      Icon(
+        Icons.star,
+        size: 15.0,
+        color: Colors.yellow[600],
+      ),
+      Icon(
+        Icons.star,
+        size: 15.0,
+        color: Colors.black,
+      ),
+      Icon(
+        Icons.star,
+        size: 15.0,
+        color: Colors.black,
+      ),
+    ],
+  );
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Text(
+        'Info with openweathermap.org',
+        style: TextStyle(
+          fontSize: 15.0,
+        ),
+      ),
+      stars,
+    ],
   );
 }
